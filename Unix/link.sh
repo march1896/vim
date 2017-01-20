065fe4bc30c1c88d9132591368d5c1d00ae08bb7
Jin Tan <jintan@owtware.com>
2017-01-20 16:06:31 +0800

    add deploy link.sh

---
+++ Unix/link.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+ln -s -f $(pwd)/.bashrc ~/.bashrc
+ln -s -f $(pwd)/.gitconfig ~/.gitconfig
+ln -s -f $(pwd)/.inputrc ~/.inputrc
+ln -s -f $(pwd)/.pylintrc ~/.pylintrc
+ln -s -f $(pwd)/.tmux.conf ~/.tmux.conf
+ln -s -f $(pwd)/.vimrc ~/.vimrc
